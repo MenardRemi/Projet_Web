3161803c8ab3bfa6a0b447f1945cea1597b9692c
remi_mrd <remi.menard06@gmail.com>
2024-11-02 11:29:38 +0100

    php recup meilleurs scores + html leur affichage et boutton jeu

--- core-master/index.php
+++ core-master/index.php
@@ -1,15 +1,48 @@
 <?php
 
-
-
 declare(strict_types=1);
 require 'flight/Flight.php';
 session_start();
 
 
 Flight::route('', function() {
+    $host = 'localhost'; // Adresse du serveur PostgreSQL
+    $dbname = 'postgres'; // Nom de la base de données
+    $user = 'postgres'; // Nom d'utilisateur PostgreSQL
+    $password = 'md5'; // Mot de passe PostgreSQL
 
-    Flight::render('accueil');
+    try {
+        // Création de la connexion PDO
+        $pdo = new PDO("pgsql:host=$host;port=5433;dbname=$dbname", $user, $password);
+        
+        // Configuration des options de PDO (par exemple, pour afficher les erreurs)
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Connexion réussie à PostgreSQL !";
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+    }
+
+    // Requête pour récupérer les 10 meilleurs scores
+    $sql = "SELECT joueur, temps FROM scores ORDER BY temps DESC LIMIT 10";
+    
+    // Exécution de la requête
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    // Récupération des résultats
+    $meilleursScores = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+    }
+
+    Flight::render('accueil', ['meilleursScores' => $meilleursScores]);
+});
+
+Flight::route('/jeu', function(){
+    # Récupération des objets de la bdd
+    # Redirection vers le fichier jeu.php en incluant la table des objets
 });
 
 Flight::start();
--- core-master/views/accueil.php
+++ core-master/views/accueil.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Accueil</title>
 </head>
 <body>
     <h1>La quête du disque volé</h1>
+    <a href="/jeu" class="btn btn-primary">Jouer</a>
+    <h2>Hall of fame</h2>
+    <tbody>
+        <?php foreach ($meilleursScores as $score): ?>
+            <tr>
+                <td><?= htmlspecialchars($score['joueur']); ?></td>
+                <td><?= htmlspecialchars($score['temps']); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </body>
 </html>
