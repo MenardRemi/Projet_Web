4649ccc699351e52cf790eef2df016571126f187
remi_mrd <remi.menard06@gmail.com>
2024-11-25 19:28:15 +0100

    ajout des data

--- core-master/index.php
+++ core-master/index.php
@@ -1,9 +1,10 @@
 <?php
-
+// Paramètres d'initialisation
 declare(strict_types=1);
 require_once 'flight/Flight.php';
 session_start();
 
+// Paramètres de connexion à la BDD
 $host = 'localhost';
 $port = '5433';
 $dbname = 'postgres';
@@ -16,116 +17,39 @@ $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$passwo
 // Création de la connexion
 $conn = pg_connect($conn_string);
 
+// Vérification de la connexion
 if (!$conn){
-    exit("Erreur");
-}
-else{
-
+    exit("Erreur de connexion à la base de données");
 }
 
+// Initialisation
 session_start();
-
 Flight::set('conn', $conn);
 
+
 Flight::route('GET /accueil', function() use ($conn) {
-
-
-    
-
-
-
-    // Vérification de la connexion
-    if (!$conn) {
-        echo "Erreur de connexion à la base de données.";
-    } else {
-        
-
-        /*if ($result) {
-            // Parcours des résultats
-            while ($row = pg_fetch_assoc($result)) {
-                echo "<br>" . "nom: " . $row['nom'] . "<br>";
-            }
-        } else {
-            echo "Erreur lors de la récupération des données.";
-        }*/
-
         // Requête pour récupérer les 10 meilleurs scores
         $sql = "SELECT nom, temps FROM score ORDER BY temps LIMIT 10";
         
         // Exécution de la requête
         $result = pg_query($conn, $sql);
 
-        // Récupération des résultats
-        
+        // Transmission des résultats à l'accueil
         if ($result) {
             Flight::render('accueil', ['meilleursScores' => $result]);
         } else {
             echo "Erreur lors de la récupération des données.";
         }
     }
+);
 
-
-
-
-    # Connexion au serveur
-    /*$host = 'localhost'; // Adresse du serveur PostgreSQL
-    $dbname = 'postgres'; // Nom de la base de données
-    $user = 'postgres'; // Nom d'utilisateur PostgreSQL
-    $password = 'postgres'; // Mot de passe PostgreSQL
-
-    try {
-        // Création de la connexion PDO
-        $pdo = new PDO("pgsql:host=$host;port=5433;dbname=$dbname", $user, $password);
-        
-        // Configuration des options de PDO (par exemple, pour afficher les erreurs)
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        echo "Connexion réussie à PostgreSQL !";
-
-        # Enregistrement du lien à la BDD
-        Flight::set('db', $pdo);
-
-    } catch (PDOException $e) {
-        echo "Erreur de connexion : " . $e->getMessage();
-    }
-
-    */
-    //Flight::render('accueil_2');
-});
-
-/*Flight::route('GET /map', function(){
-
-    # Reconnexion à la base de donnée
-    $pdo = Flight::get('db');
-
-    # Récupération des objets de la bdd
-    
-    # Redirection vers le fichier map.php en incluant la table des objets
-    # Le fichier javascript associé à ce fichier renverra vers la route ci-desous pour récupérer les informations des 
-    # objets nécessaires
-});*/
-
+// Route menant 
 Flight::route('GET /map', function() {
     Flight::render('jeu');
 });
 
+
 Flight::route('GET /api/objets', function() use ($conn) {
-    /*$host = 'localhost';
-    $port = '5433';
-    $dbname = 'postgres';
-    $user = 'postgres';
-    $password = 'postgres';
-
-    // Chaîne de connexion
-    $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";*/
-
-
-    if (!$conn) {
-        echo json_encode(["error" => "Erreur de connexion à la base de données"]);
-        return;
-    }
-
-
     if (isset($_GET['id']) && !empty($_GET['id'])) {
         // Requête pour récupérer l'objet sélectionné
         //$sql = "SELECT id, objet_nom, ST_X(ST_Transform(position, 3857)::geometry) AS longitude, ST_Y(ST_Transform(position, 3857)::geometry) AS latitude, zoom, icone, bloque, texte, code, remove, recuperable FROM objets WHERE id=" . pg_escape_string($_GET['id']);
